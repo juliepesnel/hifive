1405be3cf65fc6ab5b65bb340eb4cf399b465bca
juliepesnel <juliepesnel@gmail.com>
2017-02-28 13:06:58 +0100

    participation model and db changed

--- db/migrate/20170228104828_create_participations.rb
+++ db/migrate/20170228104828_create_participations.rb
@@ -1,7 +1,7 @@
 class CreateParticipations < ActiveRecord::Migration[5.0]
   def change
     create_table :participations do |t|
-      t.boolean :confirmation
+      t.column :status, :integer, default: 0
       t.references :user, foreign_key: true
 
       t.timestamps
