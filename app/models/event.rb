dd2d88fb1c6acf29409fc48ff2b4b87d352df29f
macbook <petitm31@gmail.com>
2017-02-28 16:16:51 +0100

    db seed okay

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
-  has_many :participations
+  has_many :participations, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Restaurant.destroy_all
+User.destroy_all
+
+categories = %w(french italian thaï chinese fast-food)
+categories.each { |category| Category.create!(name: category) }
+
+restaurants = [
+  {
+    name: "Le Pressoir d'Argent",
+    address: "2, place de la Comédie",
+    phone_number: "05.56.33.33.33",
+    website: "https://www.tripadvisor.com",
+    category: Category.all.sample,
+    },
+
+  {
+    name: "Restaurant Côté Rue",
+    address: "14, rue Paul Louis Lande",
+    phone_number: "05.56.33.33.33",
+    website: "https://www.tripadvisor.com",
+    category: Category.all.sample,
+    },
+
+  {
+    name: "Dragon Doré",
+    address: "9, rue Paul Louis Lande",
+    phone_number: "05.56.33.33.33",
+    website: "https://www.tripadvisor.com",
+    category: Category.all.sample,
+    },
+
+  {
+    name: "Restaurant Mélodie",
+    address: "6, rue des Faussets",
+    phone_number: "05.56.33.33.33",
+    website: "https://www.tripadvisor.com",
+    category: Category.all.sample,
+  }
+]
+
+restaurants.each { |restaurant| Restaurant.create!(restaurant) }
+
+users = [
+
+{
+ first_name: "Jean-Marc",
+ last_name: "Le Pen",
+ email: "JM@gmail.com",
+ password: "123456",
+ },
+
+{
+ first_name: "Marine",
+ last_name: "Le Pen",
+ email: "marine@gmail.com",
+ password: "123456",
+ },
+
+{
+ first_name: "Jules",
+ last_name: "Le Pan",
+ email: "Jules@gmail.com",
+ password: "123456",
+ },
+
+{
+ first_name: "Lucie",
+ last_name: "Le Don",
+ email: "lucie@gmail.com",
+ password: "123456",
+ }
+]
+
+users.each {|user| User.create!(user)}
