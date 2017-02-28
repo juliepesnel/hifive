c45cc73165c4dc565d8eb2b25548bd428c2ef3ba
macbook <petitm31@gmail.com>
2017-02-28 12:19:24 +0100

    Modify links

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+  has_many :participations
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
   belongs_to :category
+  has_many :events
 end
