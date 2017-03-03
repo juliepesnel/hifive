3a2c0a7cd3632efbf18f266c77f9d66422a93c99
macbook <petitm31@gmail.com>
2017-03-03 15:59:54 +0100

    Selected checkboxe improve

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def avatar_image_tag user, options = {}
-    if current_user.picture
-      cl_image_tag current_user.picture.path, options
-    elsif current_user.facebook_picture_url
-      image_tag current_user.facebook_picture_url, options
+    if user.picture
+      cl_image_tag user.picture.path, options
+    elsif user.facebook_picture_url
+      image_tag user.facebook_picture_url, options
     else
       image_tag "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/286.jpg", options
     end
