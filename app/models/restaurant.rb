6cbda720cdeed030a84d88399b988539d9e845c2
macbook <petitm31@gmail.com>
2017-03-08 14:27:41 +0100

    Add API google map

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :events, dependent: :destroy
-   geocoded_by :address
   belongs_to :category
+
+  geocoded_by :address
   after_validation :geocode, if: :address_changed?
 end
