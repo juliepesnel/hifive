de26dccd114af798c20b0c2b2551825d9f2870d7
macbook <petitm31@gmail.com>
2017-03-09 12:15:22 +0100

    Twillo install

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root to: 'pages#home'
   resources :events
   resources :tastes, only: [:new, :create]
   resources :participations, only: [:update]
+  post 'twilio/sendsms' => 'twilio#sendsms'
 
   mount Attachinary::Engine => "/attachinary"
+
 end
