5956b8bff16da38c7afac322f7b6cf4c9427aa18
Thibault Lanery <thibaultlanery@gmail.com>
2017-03-08 21:57:51 -0800

    illusion d'optique loader ok

---
+++ app/assets/javascripts/loader.js
@@ -0,0 +1,13 @@
+$( ".selectable_restaurants" ).hide();
+$( ".loader" ).hide();
+
+
+
+$( ".myflextest input" ).click(function() {
+  $( ".loader" ).show();
+  $( ".selectable_restaurants" ).hide();
+  setTimeout(function() {
+   $('.loader').fadeOut('fast');
+   $('.selectable_restaurants').fadeIn('fast');
+  }, 2000);
+});
